1d8fcfa7bdb307f9aed61b5cf403a31a6e4fdaa9
farrahnurmalia <74038303+farrahnurmalia@users.noreply.github.com>
2023-08-20 23:46:49 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,97 @@
+<script>
+  import CircleElements from './CircleElements.svelte'
+  import { onMount } from 'svelte'
+  import { gender } from './data.js'
+
+  const circleDots = [
+    { color: 'red', top: 30, left: 50, text: 'MALE' },
+    { color: 'yellow', top: 90, left: 50, text: 'FEMALE' },
+    { color: 'green', top: 30, left: 200, text: 'BOTH' },
+    { color: 'blue', top: 90, left: 200, text: 'UNDISCLOSED' }
+  ];
+
+ let genderCounts = {};
+
+  onMount(() => {
+    genderCounts = calculateGenderCounts(gender);
+  });
+
+  function calculateGenderCounts(data) {
+  const counts = {};
+
+  data.forEach(item => {
+    const year = item.year;
+    const gender = item.author_gender;
+
+    if (!counts[year]) {
+      counts[year] = {};
+    }
+
+    if (!counts[year][gender]) {
+      counts[year][gender] = 0;
+    }
+
+    counts[year][gender]++;
+  });
+
+  return counts;
+}
+</script>
+
+<div class="horizontal-bar-chart">
+  {#each Object.entries(genderCounts) as [year, counts]}
+    <div class="bar-container">
+      <div class="year-label">{year}</div>
+      <div class="bar">
+        {#each Object.entries(counts) as [gender, count]}
+          <div
+            class="bar-segment"
+            style="width: {count * 10}px; background-color: {
+              gender === 'Male' ? '#990000' :
+              gender === 'Female' ? '#FEBE00' :
+              gender === 'Both' ? '#5B7742' :
+              gender === 'Undisclosed' ? '#0081DD' :
+              'gray'
+            }"
+          >
+            {gender}: {count}
+          </div>
+        {/each}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .horizontal-bar-chart {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .bar-container {
+    margin: 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .year-label {
+    font-size: 14px;
+    margin-right: 10px;
+  }
+
+  .bar {
+    display: flex;
+    align-items: center;
+  }
+
+  .bar-segment {
+    height: 20px;
+    margin-right: 5px;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 12px;
+    padding: 0 5px;
+  }
+</style>
---
+++ src/data.js
@@ -0,0 +1,842 @@
+export const gender = [
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2018,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Both"
+  },
+  {
+    "year": 2019,
+    "author_gender": "Both"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Both"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Both"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Undisclosed"
+  },
+  {
+    "year": 2020,
+    "author_gender": "Undisclosed"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2021,
+    "author_gender": "Both"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Male"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Female"
+  },
+  {
+    "year": 2022,
+    "author_gender": "Both"
+  }
+]
